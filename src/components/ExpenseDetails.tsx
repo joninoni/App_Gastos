5272ff8db7d644af3511678b501fc10110c94856
joninoni <jonibeltranhayer@gmail.com>
2024-05-15 13:52:46 -0600

    agrego la accion de eliminar y añado un efecto swippe

--- src/components/ExpenseDetails.tsx
+++ src/components/ExpenseDetails.tsx
@@ -1,6 +1,11 @@
 import AmountDisplay from "./AmountDisplay"
+import {SwipeableList,SwipeableListItem,LeadingActions,TrailingActions,SwipeAction} from "react-swipeable-list"
+import "react-swipeable-list/dist/styles.css"
 import { categories } from "../data/categories"
 import { Expense } from "../types"
+import { useBudget } from "../hooks/useBudget"
+
+
 type ExpenseDetailsProps = {
     expense : Expense
 }
@@ -8,32 +13,58 @@ type ExpenseDetailsProps = {
 
 const ExpenseDetails = ({expense} : ExpenseDetailsProps) => {
 
+    const {dispatch} = useBudget()
+
+    const leadingActions = () => (
+        <LeadingActions>
+            <SwipeAction onClick={() => {}}>
+                Editar
+            </SwipeAction>
+        </LeadingActions>
+    )
+    
+    const trailingActions =  () => (
+        <TrailingActions>
+            <SwipeAction onClick={ () => dispatch({type:"remove-expense",payload:{id : expense.id}})}>
+                Eliminar
+            </SwipeAction>
+        </TrailingActions>
+    )
+
     const categoryInfo = categories.filter( cat => cat.id === expense.category)[0]
    
     return (
-        <div className="bg-white shadow-lg p-10 w-full border border-gray-200 flex gap-5 items-center">
-            <div>
-                <img className=" w-20" src={`../../public/img/icono_${categoryInfo.icon}.svg`} alt="icono de categoria"/>
-            </div>
-            <div className="flex-1 space-y-2">
-                <p className="text-sm font-bold uppercase text-slate-500">
-                    <span className="font-bold text-black">Categoria: {""}</span>
-                    {categoryInfo.name}
-                </p>
-                <p className="text-sm font-bold uppercase text-slate-500 ">
-                    <span className="font-bold text-black">Nombre: {""}</span>
-                    {expense.expenseName}
-                </p>
-                <p className="text-slate-600 text-sm">
-                    <span className="font-bold text-black">Fecha: {""}</span>
-                    {expense.date}
-                </p>
-            </div>
+        <SwipeableList>
+            <SwipeableListItem
+                maxSwipe={30}
+                trailingActions={trailingActions()}
+                leadingActions={leadingActions()}
+            >
+                <div className="bg-white shadow-lg p-10 w-full border border-gray-200 flex gap-5 items-center">
+                    <div>
+                        <img className=" w-20" src={`../../public/img/icono_${categoryInfo.icon}.svg`} alt="icono de categoria"/>
+                    </div>
+                    <div className="flex-1 space-y-2">
+                        <p className="text-sm font-bold uppercase text-slate-500">
+                            <span className="font-bold text-black">Categoria: {""}</span>
+                            {categoryInfo.name}
+                        </p>
+                        <p className="text-sm font-bold uppercase text-slate-500 ">
+                            <span className="font-bold text-black">Nombre: {""}</span>
+                            {expense.expenseName}
+                        </p>
+                        <p className="text-slate-600 text-sm">
+                            <span className="font-bold text-black">Fecha: {""}</span>
+                            {expense.date}
+                        </p>
+                    </div>
 
-            <AmountDisplay
-                amount={expense.amount}
-            />
-        </div>
+                    <AmountDisplay
+                        amount={expense.amount}
+                    />
+                </div>
+            </SwipeableListItem>
+        </SwipeableList>
     )
 }
 
